2e64cd04d5cd3d74eaf2e4bee6ab6a30e0a2e9f3
Dominika <dominika.jansova1011@gmail.com>
2021-03-12 19:53:24 +0100

    setInterval

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const $hours = document.getElementById('hours');
 const $minutes = document.getElementById('minutes');
 const $seconds = document.getElementById('seconds');
 
+const COVID_INCEPTION_IN_CZ = '2020/03/1 00:00:00';
 
 function timeDiffCalc(dateFuture, dateNow) {
     const SECONDS_PER_DAY = 86400;
@@ -16,22 +17,18 @@ function timeDiffCalc(dateFuture, dateNow) {
     // calculate days
     const days = Math.floor(diffInMilliSeconds / SECONDS_PER_DAY);
     diffInMilliSeconds -= days * SECONDS_PER_DAY;
-    console.log('calculated days', days);
 
     // calculate hours
     const hours = Math.floor(diffInMilliSeconds / MILISEC_IN_HOUR) % HOURS_PER_DAY;
     diffInMilliSeconds -= hours * MILISEC_IN_HOUR;
-    console.log('calculated hours', hours);
 
     // calculate minutes
     const minutes = Math.floor(diffInMilliSeconds / SECONDS) % MINUTES_PER_HOUR;
     diffInMilliSeconds -= minutes * SECONDS;
-    console.log('minutes', minutes);
 
     // calculate seconds
     const seconds = Math.floor(diffInMilliSeconds) % MINUTES_PER_HOUR;
     diffInMilliSeconds -= seconds * SECONDS;
-    console.log('seconds', seconds);
 
     return {
       days,
@@ -41,11 +38,14 @@ function timeDiffCalc(dateFuture, dateNow) {
     };
 }
 
-  // console.log(timeDiffCalc(new Date(), new Date('2020/03/1 00:00:00')));
+    setInterval(function() { 
+      const { days, hours, minutes, seconds } = timeDiffCalc(new Date(), new Date(COVID_INCEPTION_IN_CZ));
+      $days.innerHTML = days;
+      $hours.innerHTML = hours;
+      $minutes.innerHTML =  minutes;
+      $seconds.innerHTML = seconds;
+   }, 1000);
 
-  const { days, hours, minutes, seconds } = timeDiffCalc(new Date(), new Date('2020/03/1 00:00:00'));
 
-  $days.innerHTML = days;
-  $hours.innerHTML = hours;
-  $minutes.innerHTML =  minutes;
-  $seconds.innerHTML = seconds;
+
+  
