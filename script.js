0742b9002e08a2bd5cac0ed934932132cfbe084d
Dominika <dominika.jansova1011@gmail.com>
2021-03-14 17:40:57 +0100

    Created object inflectionTranslations and function getTimeTranslation

--- script.js
+++ script.js
@@ -11,17 +11,14 @@ const $minutesUnit = document.getElementById('minutes-unit');
 const $secondsUnit = document.getElementById('seconds-unit');
 
 const COVID_INCEPTION_IN_CZ = '2020/03/1 00:00:00';
+const REFRESH_RATE = 1000; 
 
 function timeDiffCalc(dateFuture, dateNow) {
     const MIN_IN_SEC = 60;
     const HOUR_IN_SEC = 60 * MIN_IN_SEC;
     const DAY_IN_SEC = 24 * HOUR_IN_SEC;
     const YEAR_IN_SEC = 365 * DAY_IN_SEC;
-
     const SECONDS_PER_MINUTE = 60;
-    // const MINUTES_PER_HOUR = 60;
-    // const HOURS_PER_DAY = 24;
-    // const DAYS_PER_YEAR = 365.242199;
 
     let diffInSeconds = Math.abs(dateFuture - dateNow) / 1000;
     
@@ -57,57 +54,76 @@ function timeDiffCalc(dateFuture, dateNow) {
       const { years, days, hours, minutes, seconds } = timeDiffCalc(new Date(), new Date(COVID_INCEPTION_IN_CZ));
       
       $years.innerHTML = years;
-        if (years <= 1) {
-          $yearsUnit.innerText = 'rok'
-        } else if (years > 1 && years < 5) {
-          $yearsUnit.innerText = 'roky'
-        } else if (years >= 5) {
-          $yearsUnit.innerText = 'let'
-        }
+      $yearsUnit.innerText = getTimeTranslation(years, 'years'); // should return e.g. 'rok'
 
-      $days.innerHTML = days;
-      if (days <= 1) {
-        $daysUnit.innerText = 'den'
-      } else if (days > 1 && days < 5) {
-        $daysUnit.innerText = 'dny'
-      } else if (days >= 5) {
-        $daysUnit.innerText = 'dnů'
-      }
+      $days.innerHTML = days;   
+      $daysUnit.innerText = getTimeTranslation(days, 'days');
 
       $hours.innerHTML = hours;
-      if (hours <= 1) {
-        $hoursUnit.innerText = 'hodina'
-      } else if (hours > 1 && hours < 5) {
-        $hoursUnit.innerText = 'hodiny'
-      } else if (hours >= 5) {
-        $hoursUnit.innerText = 'hodin'
-      }
+      $hoursUnit.innerText = getTimeTranslation(hours, 'hours');
 
       $minutes.innerHTML =  minutes;
-      if (minutes <= 1) {
-        $minutesUnit.innerText = 'minuta'
-      } else if (minutes > 1 && minutes < 5) {
-        $minutesUnit.innerText = 'minuty'
-      } else if (minutes >= 5) {
-        $minutesUnit.innerText = 'minut'
-      }
+      $minutesUnit.innerText = getTimeTranslation(minutes, 'minutes');
 
       $seconds.innerHTML = seconds;
-      if (seconds <= 1) {
-        $secondsUnit.innerText = 'sekunda'
-      } else if (seconds > 1 && seconds < 5) {
-        $secondsUnit.innerText = 'sekundy'
-      } else if (seconds >= 5) {
-        $secondsUnit.innerText = 'sekund'
-      }
-   }, 1000);
+      $secondsUnit.innerText = getTimeTranslation(seconds, 'seconds');
+   }, REFRESH_RATE);
 
    setTimeout(() => {
     $loader.remove();
     countdown.style.visibility = 'visible';
-  }, 1000);
+  }, REFRESH_RATE);
 
-  
+  const inflectionTranslations = {
+    years: {
+      0: 'let',
+      1: 'rok',
+      2: 'roky',
+    },
+    days: {
+      0: 'dnů',
+      1: 'den',
+      2: 'dny',
+    }, 
+    months: {
+      0: 'měsíců',
+      1: 'měsíc',
+      2: 'měsíce',
+    },
+    hours: {
+      0: 'hodin',
+      1: 'hodina',
+      2: 'hodiny',
+    },
+    minutes: {
+      0: 'minut',
+      1: 'minuta',
+      2: 'minuty',
+    },
+    seconds: {
+      0: 'sekund',
+      1: 'sekunda',
+      2: 'sekundy',
+    }
+  };
+
+  // value = 77
+  // timeUnit = 'years'
+
+  function getTimeTranslation(value, timeUnit) {
+    let timeIndex = 0;
+    if (value === 1) {
+      timeIndex = 1;
+    } else if (value > 1 && value < 5) {
+      timeIndex = 2;
+    } else if (value >= 5 || value === 0) {
+      timeIndex = 0;
+    }
+    return inflectionTranslations[timeUnit][timeIndex];
+  }
+
+
+
   //  const test = () => {
   //     console.log('Testing timeDiffCalc should return correct number of days');
 
@@ -122,5 +138,3 @@ function timeDiffCalc(dateFuture, dateNow) {
   //     }
   //  }
   //  test();
-
-  
